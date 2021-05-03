c438ec26d0e16f75fe8d67fdb0c382267f4a2872
csae8092 <peter.andorfer@oeaw.ac.at>
2021-05-03 13:46:32 +0200

    some css fun

--- archiv/views.py
+++ archiv/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import requests
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from archiv.models import FrdWork
+
+
+class HomePageView(TemplateView):
+
+    template_name = "archiv/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['works'] = FrdWork.objects.all()
+        return context
+
+
+class ImprintView(TemplateView):
+    template_name = 'archiv/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        r = requests.get("https://shared.acdh.oeaw.ac.at/acdh-common-assets/api/imprint.php?serviceID=19058")
+
+        if r.status_code == 200:
+            context['imprint_body'] = f"{r.text}"
+        else:
+            context['imprint_body'] = """
+            On of our services is currently not available. Please try it later or write an email to
+            acdh@oeaw.ac.at
+            """
+        return context

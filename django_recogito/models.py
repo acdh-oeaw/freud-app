83fbf06a13afac514a4774667d6401f60b0f0635
csae8092 <peter.andorfer@oeaw.ac.at>
2021-05-15 20:30:39 +0200

    annotation model

--- django_recogito/models.py
+++ django_recogito/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class RecogitoAnnotation(models.Model):
+    re_id = models.CharField(max_length=100)
+    re_text = models.TextField(blank=True, null=True)
+    re_start = models.IntegerField(blank=True, null=True)
+    re_end = models.IntegerField(blank=True, null=True)
+    re_payload = models.JSONField(blank=True, null=True)
+    re_app = models.CharField(max_length=100, blank=True, null=True)
+    re_model = models.CharField(max_length=100, blank=True, null=True)
+    re_field_name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.re_text} ({self.re_id})"
